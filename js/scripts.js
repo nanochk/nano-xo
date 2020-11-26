cef48ca8afc2233a4907504d1b969539b9b64059
nanochk <74730624+nanochk@users.noreply.github.com>
2020-11-26 08:40:48 +0100

    Update AI

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,17 @@ const game = (() => {
 	const squares = document.getElementsByClassName('square')
 	const controllerElement = document.getElementsByClassName('controller')[0]
 	const winningConditions = [
-	    [0,1,2],[3,4,5],
-	    [6,7,8],[0,3,6],
-	    [1,4,7],[2,5,8],
-	    [0,4,8],[2,4,6]
+	    [0, 1, 2], [3, 4, 5],
+	    [6, 7, 8], [0, 3, 6],
+	    [1, 4, 7], [2, 5, 8],
+	    [0, 4, 8], [2, 4, 6]
 	]
 
 	let xIndex = []
 	let oIndex = []
 	let currentPlayer
 	let robotPlayer
+	let availableMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 	const handleClick = (mode) => {
 		robotPlayer = (mode === 1) ? true : false
@@ -26,8 +27,8 @@ const game = (() => {
 	}
 
 	const updateStatus = (statusElement, message, button) => {
-		const controllerMarkup = button ? 
-		`<p>${message}</p><button onClick='game.handleClick(2)'>New Game</button> <button onClick='game.handleClick(1)'>New Game - Man vs Machine</button>` 
+		const controllerMarkup = button 
+		? `<p>${message}</p><button onClick='game.handleClick(2)'>New Game</button> <button onClick='game.handleClick(1)'>New Game - Man vs Machine</button>` 
 		: `<p>${message}</p>`
 		statusElement.innerHTML = controllerMarkup
 	}
@@ -47,63 +48,110 @@ const game = (() => {
 	const startGame = (mode) => {
 		setClearState()
 		clearBoard()
+		availableMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8]		
 		robotPlayer = (mode === true) ? true : false
-		robotPlayer ? 
-					  updateStatus(controllerElement, `New game against the computer. <br />${currentPlayer.toUpperCase()} has the first move. `, 1) 
-					: updateStatus(controllerElement, `New two player game. <br />${currentPlayer.toUpperCase()} has the first move. `, 1)
+		robotPlayer 
+				? updateStatus(
+				  	controllerElement, 
+				  	`New game against the computer. <br />${currentPlayer.toUpperCase()} has the first move. `, 
+				  	1
+		  		) 
+				: updateStatus(
+					controllerElement, 
+					`New two player game. <br />${currentPlayer.toUpperCase()} has the first move. `, 
+					1
+				)
 	}
 
 	const updatePlayer = () => currentPlayer === 'x' ? 'o' : 'x'
 
-	const calculateNextMove = () => {
-		let availableMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-		availableMoves = availableMoves.filter(n => ![...xIndex, ...oIndex].includes(n))
+	const minimax = (newBoard, player) => {
 
-		let playerMoves = oIndex
-		if (availableMoves.length === 8) {
-			let randomMove
-			randomMove = availableMoves[Math.floor(Math.random() * availableMoves.length)]
-			return(randomMove)
+		let moves = []
+		let bestMove
+		let availableSpots = availableMoves.filter(s => typeof s == 'number')
+
+		if (minimaxWin(newBoard, 'x')) {
+			return { score: -10 }
+		}
+		else if (minimaxWin(newBoard, 'o')) {
+			return { score: 10 }
+		} 
+		else if (availableSpots.length === 0) {
+			return { score: 0 }
 		}
 
-		let offensiveMove = calculateConditions(availableMoves, playerMoves)
-		if (offensiveMove) {
-			return offensiveMove
-		}
+		for (let i = 0; i < availableSpots.length; i++) {
+			let move = {}
+			move.index = newBoard[availableSpots[i]]
+			newBoard[availableSpots[i]] = player
 
-		playerMoves = xIndex
-		let defensiveMove = calculateConditions(availableMoves, playerMoves)
-		if (defensiveMove) {
-			return defensiveMove
-		}
-
-		return availableMoves[Math.floor(Math.random() * availableMoves.length)]
-	}
-
-	const calculateConditions = (availableMoves, playerMoves) => {
-		const availableMovesFilter = (move) => !availableMoves.includes(move)
-		const possibleMovesFilter = (move) => !possibleMove.includes(move)
-
-		for (let moves of winningConditions) {
-			let tempMove
-			for (i = 0, matchingMoves = 0; i < playerMoves.length; i++) {
-				moves.includes(playerMoves[i]) ? matchingMoves++ : ''
+			if (player === 'x') {
+				let result = minimax(newBoard, 'o')
+				move.score = result.score
+			} 
+			else {
+				let result = minimax(newBoard, 'x')
+				move.score = result.score
 			}
-			if (matchingMoves === 2) {
-				possibleMove = moves.filter(availableMovesFilter)
-				tempMove = moves.filter(possibleMovesFilter)
-				if (tempMove.length > 0) {
-					return tempMove
+			newBoard[availableSpots[i]] = move.index
+			moves.push(move)
+		}
+
+		if(player === 'o') {
+			let bestScore = -10000
+			for(let i = 0; i < moves.length; i++) 
+			{
+				if (moves[i].score > bestScore) 
+				{
+					bestScore = moves[i].score
+					bestMove = i
+				}
+			}
+		} 
+		else {
+			let bestScore = 10000
+			for(let i = 0; i < moves.length; i++) 
+			{
+				if (moves[i].score < bestScore)
+				{
+					bestScore = moves[i].score
+					bestMove = i
 				}
 			}
 		}
-		return false
+		return moves[bestMove]
+	}	
+
+	const minimaxWin = (board, player) => {
+		let results = []
+		for (let i = 0; i < board.length; i++) {
+		  const e = board[i];
+		  if (e === player) {
+		    results = results.concat(i);
+		  }
+		}
+		let plays = results
+		if (plays.length < 3) {
+			return null
+		}
+
+		let gameWon = null
+		for (let [index, win] of winningConditions.entries()) {
+			if (win.every(elem => plays.indexOf(elem) > -1)) {
+				gameWon = {index: index, player: player}
+				break
+			}
+		}
+		return gameWon
 	}
-	
+
 	const computerMove = () => {
 		let roboMove
-		roboMove = calculateNextMove()
-		squares[roboMove].click()	
+		fillIndexMarkers('x', xIndex)
+		fillIndexMarkers('o', oIndex)
+		roboMove = minimax(availableMoves, currentPlayer)
+		squares[roboMove.index].click()	
 	}
 	
 	const markerElement = (player) => {
@@ -191,6 +239,15 @@ const game = (() => {
 		)		
 	}
 
+	const fillIndexMarkers = (marker, index) => {
+		if (index.length > 0) {
+			index.map(i => { 
+					availableMoves[i] = marker
+				}
+			)					
+		}
+	}
+
 	const refreshBoard = () => {
 	  	xIndex = state.xIndex
 	  	oIndex = state.oIndex
@@ -212,4 +269,4 @@ window.onpopstate = (event) => {
 	}
 }
 
-game.startGame()
+game.startGame(true)
